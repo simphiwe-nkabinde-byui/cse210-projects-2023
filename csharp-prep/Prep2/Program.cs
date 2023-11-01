a8a4e1badef3b2376838e405fa0f6a49c66af7ba
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2023-11-01 20:11:36 +0200

    W01 Prove Prep2 - Conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+        string letter = "";
+        string sign = "";
+
+        // letter
+        Console.Write("Grade: ");
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // sign
+        if (grade < 97 && grade >= 60)
+        {
+            if (grade % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (grade % 10 <= 3)
+            {
+                sign = "-";
+            }
+        }
+
+        Console.WriteLine($"Grade: {letter}{sign}");
+
+        //pass or fail
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, You've passed");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Try harder next time?");
+        }
     }
 }
