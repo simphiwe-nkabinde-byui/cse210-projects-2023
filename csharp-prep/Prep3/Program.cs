d84899188b87d96b55ce1fd604656461ecd8887c
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2023-11-07 23:05:44 +0200

    W01 Prove Prep3 - Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "";
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1, 101);
+            int guess = 0;
+            int guessCount = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNum)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"{guessCount} guesses");
+
+                    Console.WriteLine("Do you want to play again?");
+                    playAgain = Console.ReadLine();
+                }
+
+            } while (guess != magicNum);
+
+        } while (playAgain == "yes");
+
+
+
+
     }
 }
