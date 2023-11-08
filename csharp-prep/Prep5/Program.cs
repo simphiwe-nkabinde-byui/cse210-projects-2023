7cfb7aaf9e85b532853356eb71f732dcfb86861e
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2023-11-08 16:08:58 +0200

    W01 Prove Prep5 - Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string username = PromptUserName();
+        int userNum = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNum);
+        DisplayResult(username, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    static void DisplayResult(string name, int squaredNum)
+    {
+        Console.Write($"{name}, the square of your number is {squaredNum}");
     }
 }
