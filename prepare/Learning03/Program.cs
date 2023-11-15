8a3dd8c6739cbfa4234fa18571bc2612163ca89f
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2023-11-15 15:06:31 +0200

    W03 Prepare Learning03 - Encapsulation

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fract1 = new Fraction();
+        Fraction fract2 = new Fraction(5);
+        Fraction fract3 = new Fraction(3, 4);
+        Fraction fract4 = new Fraction(1, 3);
+
+        Console.WriteLine(fract1.GetFractionString());
+        Console.WriteLine(fract1.GetDecimalValue());
+
+        Console.WriteLine(fract2.GetFractionString());
+        Console.WriteLine(fract2.GetDecimalValue());
+
+        Console.WriteLine(fract3.GetFractionString());
+        Console.WriteLine(fract3.GetDecimalValue());
+
+        Console.WriteLine(fract4.GetFractionString());
+        Console.WriteLine(fract4.GetDecimalValue());
     }
 }
